f72aed2012b9efd298875f91b2ead08dd892d77c
hungryDev399 <mohammedabdelaziz399@gmail.com>
2023-12-02 17:44:25 +0200

    Adding the initial headers for bus and passenger classes

---
+++ headers/passenger.h
@@ -0,0 +1,35 @@
+#include <string>
+enum class PassengerType
+{
+    Aged,
+    POD,
+    Pregenant,
+    WP,
+    NP
+};
+
+class Passenger
+{
+public:
+    // constructor
+    Passenger(char *id, int start_station, int end_station, PassengerType type);
+    Passenger();
+    // setters
+    void setCurrentWaitTime(int current_wait_time);
+
+    // getters
+    char *getId();
+    int getStartStation();
+    int getEndStation();
+    int getCurrentWaitTime();
+    PassengerType getType();
+
+    // methods
+
+private:
+    char *id = new char[1000];
+    int start_station;
+    int end_station;
+    int current_wait_time;
+    PassengerType type;
+};
